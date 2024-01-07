0125cdd3d0c9096a85e4313d25209a721bc479ad
Aariaanit <arianittershnjaku@gmail.com>
2024-01-07 15:36:09 +0100

    07.01.2023

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './src/screens/HomeScreen';
 import AboutScreen from './src/screens/AboutScreen';
