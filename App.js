86bfe021e915df06bfe7e4231252b82f799f2607
Aariaanit <arianittershnjaku@gmail.com>
2026-01-09 14:42:08 +0100

    0106

--- App.js
+++ App.js
@@ -1,37 +1,32 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import HomeScreen from './src/screens/HomeScreen';
-import AboutScreen from './src/screens/AboutScreen';
+import HomeScreen from "./src/screens/HomeScreen";
+import AboutScreen from "./src/screens/AboutScreen";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  
   return (
     <NavigationContainer>
       <Stack.Navigator
-        // screenOptions={{
-        //   header: () => null
-        // }}
+        options={{
+          header: () => "Home Screen",
+        }}
       >
         <Stack.Screen
           name="Home Screen"
           component={HomeScreen}
-        options={{
-          header: () => null
-        }}
+          options={{
+            header: () => "Home Screen",
+          }}
         />
-        <Stack.Screen
-          name="About Screen"
-          component={AboutScreen}
-        />
+        <Stack.Screen name="About Screen" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default App;
